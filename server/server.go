f5acfb572a46ece6ed634ccc99c19eaf7fc90098
Ayaan <ayaan@pop-os.localdomain>
2022-03-10 16:43:29 +0530

    Messed around with code, dont mind commit

--- server/server.go
+++ server/server.go
@@ -1,25 +1,24 @@
 package server
 
-import "gosrc.io/xmpp"
-import "os"
+import (
+	"fmt"
+	"os"
 
-import "fmt"
+	"gosrc.io/xmpp"
+)
 
-func Serverprint(){
-    // config file for connection, yeah i know its part of client, this is just
-    // the genesis commit
+func Serverprint() {
+	// config file for connection, yeah i know its part of client, this is just
+	// the genesis commit
 	config := xmpp.Config{
 		TransportConfiguration: xmpp.TransportConfiguration{
 			Address: "localhost:5222",
 		},
 		Jid:          "test@localhost",
-		Credential:   xmpp.Password("test"),
+		Credential:   xmpp.Password("newtest"),
 		StreamLogger: os.Stdout,
 		Insecure:     true,
 		// TLSConfig: tls.Config{InsecureSkipVerify: true},
 	}
-    fmt.Print(config);
+	fmt.Print(config)
 }
-
-
-
