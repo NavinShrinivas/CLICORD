ba0ec36c2e7549ae2819be413c947d44ef7dc17a
P K Navin Shirnivas <karupal2002@gmail.com>
2022-03-12 23:15:06 +0530

    15 mark lobby commit
    5 mark lobby commit
    
    This marks 15% done status for lobbies

--- server/lobby_funcs.go
+++ server/lobby_funcs.go
@@ -1,3 +1,21 @@
 package main 
+import "github.com/gorilla/websocket"
+import "net/http"
+import "fmt"
 
+func Lobby_main(w http.ResponseWriter, r *http.Request){
+    //first need to use upgrader to move to ws connection and also handle connections 
+    init_funcs(w,r);
+}
+
+
+func init_funcs(w http.ResponseWriter, r *http.Request){
+    conn,err := upgrader.Upgrade(w,r,nil);
+
+    if err!=nil{
+        fmt.Print("Upgrade err : %s", err);
+    }
+    defer conn.Close();
+    append_conn(conn);
+}
 
--- server/server.go
+++ server/server.go
@@ -16,6 +16,15 @@ var upgrader = websocket.Upgrader{
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
+//array of active websocket connections 
+
+//Before : for no public rollout 2048 connections should be plenty
+var active_conn = make(map[*websocket.Conn]int);
+//After : Keeping the the ds as array makes it linear time for adding and deleting 
+//connections, which imo happens all the time 
+//hence a HashMap can do this in constant time, and also removes all restrictions 
+//on number of maintained connections
+
 //----------End of globals----------
 
 
@@ -25,5 +34,6 @@ func main(){
     //HandleFunc provides the write and reader for HTTP
     log.Print("Starting server...");
     http.HandleFunc("/ping" , Ping_server);
+    http.HandleFunc("/lobby1" , Lobby_main);
     log.Fatal(http.ListenAndServe(*addr, nil))
 }
