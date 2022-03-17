b261a114bcfa2750d48fda6399279128e936d913
P K Navin Shirnivas <karupal2002@gmail.com>
2022-03-17 18:47:44 +0530

    domain

--- client/main.go
+++ client/main.go
@@ -45,7 +45,7 @@ import (
 //----------globals----------
 
 // This address is the constatnt address for the server, and use in every function
-var addr = flag.String("addr", "0.0.0.0:8080", "http service address")
+var addr = flag.String("addr", "catss.me:8080", "http service address")
 
 //messgae structure, needs a lot more config to work.
 type MsgStruct struct {
